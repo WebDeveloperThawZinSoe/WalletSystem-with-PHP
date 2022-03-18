41afbbf9bb705bd3120c2162aa26b1afc31364ce
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-03-18 22:47:20 +0630

    transaction system

--- backend.php
+++ backend.php
@@ -20,9 +20,95 @@
        
        $result = mysqli_query($connect, $sql);
        if($result){
+           $sql = "INSERT INTO balance(customer_id,ammount) VALUES ('$phone','500')";
+           mysqli_query($connect, $sql);
+           
            success_message("Registration success","login.php");
        }else{
            error_message("Registration Failure","register.php");
+       }
+    }
+
+    /* Send */
+    if(isset($_POST["send"])){
+       $reciver = htmlspecialchars($_POST["phone"]);
+       $ammount = htmlspecialchars(abs($_POST["ammount"]));
+       $pin = htmlspecialchars($_POST["pin"]);
+       $sender = $_SESSION["username"];
+
+       /* PIN */
+       $sql = "SELECT * FROM customer WHERE pin='$pin' AND phone='$sender' ";
+       $result = mysqli_query($connect,$sql);
+       if(mysqli_num_rows($result)>0){
+
+        /* Sender Balance Find */
+         $sql = "SELECT * FROM balance WHERE customer_id='$sender'";
+         $result1 = mysqli_query($connect,$sql);
+         if($result1){
+
+            /*  Resiver  */
+            $sql = "SELECT * FROM customer WHERE phone='$reciver'";
+            $result = mysqli_query($connect,$sql);
+            if(mysqli_num_rows($result)>0){
+
+                foreach($result1 as $r){
+
+                
+                    $remain_balance = $r['ammount'];
+                    if($ammount <= $remain_balance){
+                        /* Reciver */
+                       $current_amount_reciver = "";
+                       $sql = "SELECT * FROM balance WHERE customer_id='$reciver'";
+                       $result = mysqli_query($connect,$sql);
+                       if(mysqli_num_rows($result)>0){
+                           foreach($result as $r){
+                               $current_amount_reciver = $r['ammount'];
+                           }
+                       }
+   
+                       $new_ammount_at_reciver = (int) $current_amount_reciver + (int) $ammount;
+   
+                       $sql = "UPDATE balance SET ammount='$new_ammount_at_reciver' WHERE customer_id='$reciver' ";
+   
+                       $result = mysqli_query($connect,$sql);
+   
+                       /* Sender  */
+                       $current_amount_sender = "";
+                       $sql = "SELECT * FROM balance WHERE customer_id='$sender'";
+                       $result = mysqli_query($connect,$sql);
+                       if(mysqli_num_rows($result)>0){
+                           foreach($result as $r){
+                               $current_amount_sender = $r['ammount'];
+                           }
+                       }
+   
+                       $new_ammount_at_sender = $current_amount_sender - $ammount;
+   
+                       $sql2 = "UPDATE balance SET ammount='$new_ammount_at_sender' WHERE customer_id='$sender' ";
+   
+                       $result2 = mysqli_query($connect,$sql2);
+   
+                       if($result && $result2){
+                           success_message("Transaction is Success","index.php");
+                       }else{
+                           error_message("Transaction is Fail","index.php");
+                       }
+                       
+                    }else{
+                       error_message("Not enough fund","index.php"); 
+                    }
+                
+
+
+                }
+            }else{
+                error_message("Account Done Not Found","index.php"); 
+            }
+
+
+         }
+       }else{
+        error_message("PIN Code is Wrong","index.php");
        }
     }
 
@@ -46,6 +132,11 @@
 
     }
 
+    /* Send */
+    if(isset($_POST["send"])){
+        htmlspecialchars($_POST["phone"]);
+        htmlspecialchars($_POST["ammount"]);
+    }
 
     /* Function */
             /* Success message */
--- index.php
+++ index.php
@@ -12,6 +12,51 @@
     <?php
         include "alert.php";
     ?>
+    <div class="row">
+        <div class="col-md-4">
+            <?php
+                $name = $_SESSION["username"];
+                
+                $sql = "SELECT * FROM balance WHERE customer_id = '$name' ";
+                $result = mysqli_query($connect,$sql);
+                if($result){
+                    foreach($result as $r){
+                        ?>
+                            <h3><?php echo $r['ammount'] ?> Ks</h3>
+                        <?php
+                    }
+                }
+            ?>
+            <hr>
+            
+        </div>
+        <div class="col-md-8">
+            <h5>Transaction</h5>
+            <div class="well">
+                <form action="backend.php" method="POST">
+                    <div class="form-outline mb-4">
+                    <input required name="phone" type="number" id="typeEmailX-2" class="form-control form-control-lg" />
+                    <label class="form-label" for="typeEmailX-2">Phone</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                    <input required name="ammount" type="number" id="typePasswordX-2" class="form-control form-control-lg" />
+                    <label class="form-label" for="typePasswordX-2">Amount</label>
+                    </div>
+
+                    <div class="form-outline mb-4">
+                    <input required name="pin" type="number" id="typePasswordX-2" class="form-control form-control-lg" />
+                    <label class="form-label" for="typePasswordX-2">Pin</label>
+                    </div>
+
+                    <input type="submit" name="send" value="Send" class="btn btn-primary">
+                    <hr>
+                </form>
+            </div>
+            <h5>Transaction History</h5>
+        </div>
+    </div>
+
 </div>
 <?php
     include "footer.php";
