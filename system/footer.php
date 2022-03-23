73aaa9bd838894cf7eb90f48526e96b06f3c2246
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-03-23 09:35:13 +0630

    Admin Dashboard UI

---
+++ system/footer.php
@@ -0,0 +1,9 @@
+  <!--Main layout-->
+  <!-- MDB -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript" src="js/admin.js"></script>
+
+</body>
+
+</html>
